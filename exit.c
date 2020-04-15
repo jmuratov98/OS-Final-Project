7dfe8d07fdf421e084c0d645c77c488bc2d121a8
jieJayLan <lanjie3458@gmail.com>
2020-04-14 21:45:56 -0400

    print current directory

--- exit.c
+++ exit.c
@@ -1,13 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h> 
+
 int main(int argc, char* argv[]){
+
     for(int i=0;i<4;i++){
         char* argument = argv[i + 1];
         printf("History Command is %s\n", argument);
     }
-    printf("Enter 'Return key' back to main menu \n");
-    while(1)
-    {       
-        char input = getchar();
-        if(input == '\n')         
-            break;      
+
+    pid_t pid;
+    pid = fork(); // Create child 1
+
+    if (pid < 0) // Error forking
+    {
+        perror("Failed to fork a child");
+        exit(1);
     }
+    else if (pid == 0) // Child process
+    {
+        printf("\nPrint current directory\n");
+        execlp("ls", "ls", "-l", NULL);
+    }
+    else{
+        wait(NULL);
+        printf("\nEnter 'Return key' back to main menu \n");
+        while(1)
+        {       
+            char input = getchar();
+            if(input == '\n')         
+                break;      
+        }
+    }
+
 }
